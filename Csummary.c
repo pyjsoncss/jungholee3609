61edd34b933c3c56393f8409f59a8dfb6d29eacd
pyjsoncss <133950021+pyjsoncss@users.noreply.github.com>
2023-10-27 15:25:34 +0900

    Update Csummary.c

--- Csummary.c
+++ Csummary.c
@@ -1,2 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS //scanf 오류 무시  
 #include <stdio.h>  //라이브러리
+
+double %lf  int %d  float %f
+
+printf("변수x의 크기: %d\n", sizeof(x));  //자료형 크기 반환
+unsigned 양수만  signed 음수포함
+overflow 변수범위 초과
+#define TAX_RATE 0.2   기호상수
+ / 나눗셈 % 나머지
+x+=y  ==  x=x+y
+  !=  같지 않   && and   || or
+  max_value = (x > y) ? x : y;  참이면 x 거짓이면 y
+srand((time(NULL));  랜덤함수 매번 다르
